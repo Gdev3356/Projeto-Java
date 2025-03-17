1f21e0930e3150b0db0048ccd7719e9664cbf57a
Gdev3356 <gko.starkiller@gmail.com>
2025-03-17 09:30:59 -0300

    Projeto do Java

---
+++ Projeto01/src/br/com/funnybones/SupKid.java
@@ -0,0 +1,13 @@
+package br.com.funnybones;
+
+public class SupKid {
+    public static void main(String[] args) {
+        // eae
+        System.out.println("heheh");
+        /*
+        multiple comments with
+        multiple lines.
+        wow. that really ADDS up huh?
+         */
+    }
+}
